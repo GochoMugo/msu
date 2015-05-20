d4b006f1a0a2453cbd92acd94059b3370802f7c3
Gocho Mugo <mugo@forfuture.co.ke>
2015-05-20 18:35:38 +0300

    fix install script

--- get.sh
+++ get.sh
@@ -14,17 +14,17 @@ echo "⇒ cloning repo for source code"
 git clone ${GIT_URL} ${CLONE_DIR}
 
 echo "⇒ checking if ${BIN} is in path"
-echo ${PATH} | grep ${DEST} > /dev/null || {
+echo ${PATH} | grep ${BIN} > /dev/null || {
   echo "⇒ ${BIN} not in path. Adding it"
   echo "" >> ${BASHRC}
   echo "# added by msu" >> ${BASHRC}
-  echo "export PATH=${DEST}:\${PATH}" >> ${BASHRC}
+  echo "export PATH=${BIN}:\${PATH}" >> ${BASHRC}
   echo "    !! you may need to restart your terminal !!"
 }
 
 echo "⇒ copying library"
 mkdir -p ${MSU_LIB}
-cp -r lib/ ${DEST}/lib ${MSU_LIB}
+cp -r lib/ ${MSU_LIB}
 
 echo "⇒ copying executable"
 cp msu.sh ${MSU_EXE}
