2bb3f649b015c17946791548ab9bd2b4c3655aae
Gocho Mugo <mugo@forfuture.co.ke>
2015-07-17 10:06:24 +0300

    (+mod) console logging

--- lib/console.sh
+++ lib/console.sh
@@ -1,19 +1,37 @@
-# ask (and you shall be told)
+# write to the console
 #
 # Copyright (c) 2015 GochoMugo <mugo@forfuture.co.ke>
 
 msu_require colors
 
-# logs to console
+# writes to console with colors
 #  ${1}  message to write to console
 #  ${2} what color to use. 0 - info(blue), 1- success(green),
 #    2 - error(red)
 #  ${LOG_TITLE} for setting title of logging
-log() {
+function write() {
   local color=${COLOR_BLUE}
   [ ${2} ] && {
     [ ${2} -eq 1 ] && color=${COLOR_GREEN}
     [ ${2} -eq 2 ] && color=${COLOR_RED}
   }
   echo -e "${COLOR_WHITE}${LOG_TITLE:-log}: ${color}${1}${COLOR_RESET}"
+}
+
+
+# normal logging
+function log() {
+  write ${1} 0
+}
+
+
+# success logging
+function success() {
+  write ${1} 1
+}
+
+
+# error logging
+function error() {
+  write ${1} 2
 }
