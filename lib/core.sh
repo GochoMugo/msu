33240de2fe7633ad849d267206e8ff2b1a257428
Gocho Mugo <mugo@forfuture.co.ke>
2015-07-18 19:49:04 +0300

    update documentation and silence output from msu_require

--- lib/core.sh
+++ lib/core.sh
@@ -3,11 +3,15 @@
 #
 
 
+# we expect the the executable exports a variable MSU_LIB whose
+# value is the path to the library holding this and other modules
+
+
 # mod vars
 MSU_REQUIRE_LOCK= # lock to ensure we dont source an already-sourced file
 
 
-# loads all utils
+# load all modules
 msu_load() {
   ls ${MSU_LIB} | xargs source
 }
@@ -15,12 +19,14 @@ msu_load() {
 
 # require a module
 msu_require() {
-  echo ${MSU_REQUIRE_LOCK} | grep -E :${1}: || {
+  echo ${MSU_REQUIRE_LOCK} | grep -E :${1}: > /dev/null || {
     source ${MSU_LIB}/${1}.sh
     MSU_REQUIRE_LOCK=:${1}:${MSU_REQUIRE_LOCK}
   }
 }
 
+
+# run a single function
 msu_run() {
   local module=$(echo ${1} | grep -Eo ".*\." | grep -Eo "[a-Z0-9]+")
   local func=$(echo ${1} | grep -Eo "\..*" | cut -b 2- )
