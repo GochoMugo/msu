d58b0ed5886e1c2128c393f0476e0771a89130b3
Gocho Mugo <mugo@forfuture.co.ke>
2015-07-19 13:05:19 +0300

    (core) check dependencies when requiring modules
    Change: during requiring modules, dependencies can be checked to see
            if they are installed. The script won't, however, exit
            immediately since the target function may not use the
            dependency.

--- lib/core.sh
+++ lib/core.sh
@@ -3,6 +3,9 @@
 #
 
 
+set -e
+
+
 # we expect the the executable exports a variable MSU_LIB whose
 # value is the path to the library holding this and other modules
 
@@ -11,23 +14,39 @@
 MSU_REQUIRE_LOCK= # lock to ensure we dont source an already-sourced file
 
 
-# load all modules
-msu_load() {
-  ls ${MSU_LIB} | xargs source
+# check dependencies
+function msu__check_deps() {
+  for cmd in ${DEPS}
+  do
+    command -v ${cmd} > /dev/null 2>&1 || {
+      echo "warning: \`${cmd}' not found"
+    }
+  done
 }
 
 
 # require a module
-msu_require() {
+function msu_require() {
   echo ${MSU_REQUIRE_LOCK} | grep -E :${1}: > /dev/null || {
     source ${MSU_LIB}/${1}.sh
+    msu__check_deps
     MSU_REQUIRE_LOCK=:${1}:${MSU_REQUIRE_LOCK}
   }
 }
 
 
+# load all modules
+function msu_load() {
+  mods=$(ls ${MSU_LIB} | grep -Eo "^[a-Z]*")
+  for mod in ${mods}
+  do
+    msu_require ${mod}
+  done
+}
+
+
 # run a single function
-msu_run() {
+function msu_run() {
   local module=$(echo ${1} | grep -Eo ".*\." | grep -Eo "[a-Z0-9]+")
   local func=$(echo ${1} | grep -Eo "\..*" | cut -b 2- )
   msu_require ${module}
