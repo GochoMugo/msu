9419fec5d61425136ec441a3b3090e2fb64814e5
GochoMugo <mugo@forfuture.co.ke>
2016-01-22 13:33:59 +0300

    (deps) Install `a2x` in Debian-based distro

--- deps/install-deps.sh
+++ deps/install-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Installing `cabal` on different systems
+# Installing dependencies, such as `cabal`, on different systems
 
 
 echo
@@ -16,11 +16,19 @@ function has() {
   command "${1}" > /dev/null 2>&1
 }
 
+# informing user, when we are unaware of how to install a dependency
+# ${1} - main command used to install packages in a distro
+function missing_stub() {
+  echo " ! Ensure you have the command 'a2x' installed"
+  echo " ! If it's possible to install it using \'${1}\',"
+  echo " !   open up an issue, or even better, send"
+  echo " !   a pull request to the repo."
+}
 
 # Debian-based distro
 has "apt-get" && {
   sudo apt-get update -qq
-  sudo apt-get install -qq -y cabal-install
+  sudo apt-get install --no-install-recommends -qq -y cabal-install asciidoc libxml2-utils
   exit
 }
 
@@ -29,13 +37,15 @@ has "apt-get" && {
 has "yum" && {
   sudo yum update
   sudo yum install cabal-install
+  missing_stub "yum"
   exit
 }
 
 
 # openSUSE:Tumbleweed
 has "zypper" && {
-  zypper in cabal-install
+  sudo zypper in cabal-install
+  missing_stub "zypper"
   exit
 }
 
@@ -43,6 +53,7 @@ has "zypper" && {
 # Mac OS X with homebrew
 has "brew" && {
   brew install cabal-install
+  missing_stub "brew"
   exit
 }
 
@@ -50,6 +61,7 @@ has "brew" && {
 # Mac OS X with MacPorts
 has "port" && {
   port install hs-cabal-install
+  missing_stub "port"
   exit
 }
 
